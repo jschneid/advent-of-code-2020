ad63b74fcfe395e94e7ba31a0518731cb6d0c090
Jon Schneider <jon.schneider@appfolio.com>
2023-01-19 09:03:05 -0500

    Day 19 part 2

--- day-19/day-19-part-2.rb
+++ day-19/day-19-part-2.rb
@@ -4,15 +4,49 @@ def count_messages_matching_rule_0
 
   rules = build_rules(input_lines[0...divider_index])
 
-  rules['8'][:next_rules_1] = ['42']
-  rules['8'][:next_rules_2] = ['42', '8']
-  rules['11'][:next_rules_1] = ['42', '31']
-  rules['11'][:next_rules_2] = ['42', '11', '31']
+  # The rule changes for Day 19 part 2 are:
+  #   8: 42 | 42 8
+  #   11: 42 31 | 42 11 31
+  #
+  # Conveniently, our root rule, rule 0, is:
+  #   0: 8 11
+  #
+  # And rules 8 and 11 aren't referenced from anywhere else (other than rule 0).
+  #
+  # The recursiveness in rule 8 can be extrapolated out to the following pattern:
+  #   8: 42 | 42 42 | 42 42 42 | 42 42 42 42 | ...
+  #
+  # Similarly, rule 11 can be extrapolated out to this pattern:
+  #   11: 42 31 | 42 42 31 31 | 42 42 42 31 31 31 | ...
+  #
+  # So, what we can do is, pregenerate a bunch of variations of rule 0, each with
+  # a distinct combination of the non-recursive possible values of rules 8 and 11
+  # (as shown above). Then, just run our logic from Part 1 with each of those
+  # pregenerated rule 0 variations.
+  # .
+  # (Up to 10 copies of each of rules 8 and 11 is more than enough to cover all of
+  # the message strings that appear in the input.)
+  rule_0_list = []
+  (1..10).each do |i|
+    (1..10).each do |j|
+      new_rule_0_list_item = []
+      i.times { new_rule_0_list_item << '42' }
+      j.times { new_rule_0_list_item << '42' }
+      j.times { new_rule_0_list_item << '31' }
+      rule_0_list << new_rule_0_list_item
+    end
+  end
 
   messages_lines = input_lines[(divider_index+1)..]
   matches = 0
   messages_lines.each do |message_line|
-    matches += 1 if leading_characters_matching_rule(message_line, rules, '0').max == message_line.length
+    rule_0_list.each do |rule_0|
+      rules['0'][:next_rules_1] = rule_0
+      if leading_characters_matching_rule(message_line, rules, '0') == message_line.length
+        matches += 1
+        break
+      end
+    end
   end
   matches
 end
@@ -29,7 +63,6 @@ def build_rules(rule_lines)
     end
     next_rules = rule_body.split('|')
     next_rules_1 = next_rules[0].split(' ')
-    next_rules_2 = []
     next_rules_2 = next_rules[1].split(' ') unless next_rules[1].nil?
     rules[rule_id] = {
       next_rules_1: next_rules_1,
@@ -43,35 +76,29 @@ def leading_characters_matching_rule(input, rules, rule_id)
   rule = rules[rule_id]
 
   if (rule == 'a' || rule == 'b')
-    return [1] if rule == input[0]
-    return []
+    return 1 if rule == input[0]
+    return nil
   end
 
-  trimmed_inputs_1 = trim_leading_matches(rule[:next_rules_1], input, rules)
-  trimmed_inputs_2 = trim_leading_matches(rule[:next_rules_2], input, rules)
+  next_rules_list = [rule[:next_rules_1]]
+  next_rules_list << rule[:next_rules_2] unless rule[:next_rules_2].nil?
 
-  (trimmed_inputs_1 + trimmed_inputs_2).compact.uniq.map { |trimmed_input| input.length - trimmed_input.length }
+  next_rules_list.each do |next_rules|
+    trimmed_input = trim_leading_matches(next_rules, input, rules)
+    return input.length - trimmed_input.length unless trimmed_input.nil?
+  end
+
+  nil
 end
 
 def trim_leading_matches(rules_list, input, rules)
-  trimmed_inputs = [input]
-
+  trimmed_input = input
   rules_list.each do |next_rule_id|
-    new_trimmed_inputs = []
-    trimmed_inputs.each do |trimmed_input|
-      # next if trimmed_input == input
-
-      characters_matched_counts = leading_characters_matching_rule(trimmed_input, rules, next_rule_id)
-
-      characters_matched_counts.uniq.each do |characters_matched_count|
-        new_trimmed_inputs = new_trimmed_inputs + trimmed_inputs.map { |trimmed_input_0 | trimmed_input_0[characters_matched_count..] } if characters_matched_count > 0
-      end
-    end
-
-    trimmed_inputs = new_trimmed_inputs
+    characters_matched = leading_characters_matching_rule(trimmed_input, rules, next_rule_id)
+    return nil if characters_matched.nil?
+    trimmed_input = trimmed_input[characters_matched..]
   end
-
-  trimmed_inputs
+  trimmed_input
 end
 
 pp count_messages_matching_rule_0
